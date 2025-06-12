80f5a2e18b0912b1b4dcf8eb37b3ddf428cfd05e
kwhite130lbs <kwhite130lbs@yahoo.com>
2025-06-11 21:18:50 -0500

    React test render successful

--- client/src/App.js
+++ client/src/App.js
@@ -1,2 +1,12 @@
-function App() { return <div>OmniLearn React App</div>; }
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>✅ OmniLearn React is Live!</h1>
+      <p>If you see this message, your frontend is working.</p>
+    </div>
+  );
+}
+
 export default App;
